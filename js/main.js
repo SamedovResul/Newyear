71c5405dd3a7477a0c66b8ea0af577be0fc5046d
SamedovRasul <samedovrasul7@gmail.com>
2021-12-17 13:40:31 +0400

    first commit

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@
 var rellax = new Rellax('.rellax');
 
 const loading = document.querySelector('.loading')
+const one = document.querySelector('.one')
 
 setTimeout(() => {
-  loading.style.display = 'none'
+  loading.style.display = 'none';
+  one.style.display = 'flex'
 }, 3000);
